9989d90d9350098087707e2ae47a60d4bf70128d
Jason Bongiovanni <jpbon@protonmail.com>
2022-05-06 14:00:13 -0400

    config vars

--- dental/settings.py
+++ dental/settings.py
@@ -130,7 +130,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_HOST = 'smtpd.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'drj@gmail.com'
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default = '')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', '')
 EMAIL_USE_TLS = True
 Email_USE_SSL = False
 
