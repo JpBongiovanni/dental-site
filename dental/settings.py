0bd3795de6191cc7c5972bc4500d1389b1324dfe
Jason Bongiovanni <jpbon@protonmail.com>
2022-05-09 12:36:43 -0400

    hide keys

--- dental/settings.py
+++ dental/settings.py
@@ -16,8 +16,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY', '')
 
-# SECRET_KEY = "django-insecure-i9cky*wk1_@^$20_rs#_!s!t*-u+^^pq^hx8pb&+5&3s$%%#q&"
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
